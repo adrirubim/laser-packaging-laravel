ef82b09d1b6fefe77710ae5eb43be72462096431
adrirubim <adrianmorillasperez@gmail.com>
2026-04-08 11:41:47 +0200

    test: disable PreventRequestForgery in feature tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,22 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Feature tests shouldn't fail with 419 due to missing CSRF tokens.
+        // Laravel versions differ on the middleware class name.
+        $csrfMiddlewareClasses = [
+            \Illuminate\Foundation\Http\Middleware\PreventRequestForgery::class,
+            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+            \Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class,
+        ];
+
+        foreach ($csrfMiddlewareClasses as $middlewareClass) {
+            if (class_exists($middlewareClass)) {
+                $this->withoutMiddleware($middlewareClass);
+            }
+        }
+    }
 }
